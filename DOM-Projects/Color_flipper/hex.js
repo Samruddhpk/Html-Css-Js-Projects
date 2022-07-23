2ae64d9b704516411ef05cc0d76fad41c82bb634
Samruddh pk <107286956+Samruddhpk@users.noreply.github.com>
2022-07-23 14:37:02 +0530

    Update hex.js

--- DOM-Projects/Color_flipper/hex.js
+++ DOM-Projects/Color_flipper/hex.js
@@ -1,33 +1,19 @@
 const colors = ["A", "B", "C", "D", "E", "F", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-// using jquery
-$(".btn").click(function () {
+// vanilla
+
+ const btn = document.querySelector(".btn")
+const color = document.querySelector(".color")
+
+ btn.addEventListener("click", () => {
 	let hexColor = "#"
 	for (i = 0; i < 6; i++) {
 		hexColor += colors[getRandomNumber()]
 	}
-	$("body").css("background", hexColor)
-	$(".color").text(hexColor)
-})
 
-const getRandomNumber = () => {
-	return Math.floor(Math.random() * colors.length)
-}
-
-// vanilla
-
-// const btn = document.querySelector(".btn")
-// const color = document.querySelector(".color")
-
-// btn.addEventListener("click", () => {
-// 	let hexColor = "#"
-// 	for (i = 0; i < 6; i++) {
-// 		hexColor += colors[getRandomNumber()]
-// 	}
-
-// 	document.body.style.backgroundColor = hexColor
-// 	color.textContent = hexColor
-// })
-// const getRandomNumber = () => {
-// 	return Math.floor(Math.random() * colors.length)
-// }
+ 	document.body.style.backgroundColor = hexColor
+ 	color.textContent = hexColor
+ })
+ const getRandomNumber = () => {
+ 	return Math.floor(Math.random() * colors.length)
+ }
